1b1cebca2640625190066f0d3b1f2463c94a4286
Kebyn Felipe <kebyn07@gmail.com>
2025-08-19 01:28:51 -0600

    Agregando diseño

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.querySelector('.next-button').addEventListener('click', () => {
 function navigate(direction) {
     const galleryContainer = document.querySelector('.gallery-container');
     const totalImages = document.querySelectorAll('.gallery-item').length;
-    
+
     currentIndex = (currentIndex + direction + totalImages) % totalImages;
     const offset = -currentIndex * 100;
-    
+
     galleryContainer.style.transform = `translateX(${offset}%)`;
 }
 
@@ -33,7 +33,7 @@ function stopAutoplay() {
 }
 
 // Iniciar autoplay con un intervalo de 3 segundos.
-startAutoplay(3000);
+startAutoplay(5000);
 
 // Opcional: Detener autoplay cuando el usuario interactúa con los botones de navegación.
 document.querySelectorAll('.nav-button').forEach(button => {
@@ -45,25 +45,25 @@ document.querySelectorAll('.nav-button').forEach(button => {
 var origins = document.querySelectorAll("button[data-origin]");
 
 for (let i = 0; i < origins.length; i++) {
-  origins[i].addEventListener('click', function(e) {
-      // Get all elements with data-origin defined
-      var allOrigins = document.querySelectorAll("*[data-origin]");             
-      for (i = 0; i < allOrigins.length; i++) {
-          allOrigins[i].classList.remove("active");
-      }
-      
-      // Get all elements with data-target defined
-      var allTargets = document.querySelectorAll("*[data-target]");
-      for (i = 0; i < allTargets.length; i++) {
-          allTargets[i].style.display = "none";
-      }
-      
-      //Only get elements of which the data-target attribute matches the value of the data-origin of the clicked  element
-      var targets = document.querySelectorAll("*[data-target='"+e.target.dataset.origin+"']");   
-      for (i = 0; i < targets.length; i++) {
-          targets[i].style.display = "block";      
-      }
-      
-      e.target.classList.add("active");
-  });
+    origins[i].addEventListener('click', function (e) {
+        // Get all elements with data-origin defined
+        var allOrigins = document.querySelectorAll("*[data-origin]");
+        for (i = 0; i < allOrigins.length; i++) {
+            allOrigins[i].classList.remove("active");
+        }
+
+        // Get all elements with data-target defined
+        var allTargets = document.querySelectorAll("*[data-target]");
+        for (i = 0; i < allTargets.length; i++) {
+            allTargets[i].style.display = "none";
+        }
+
+        //Only get elements of which the data-target attribute matches the value of the data-origin of the clicked  element
+        var targets = document.querySelectorAll("*[data-target='" + e.target.dataset.origin + "']");
+        for (i = 0; i < targets.length; i++) {
+            targets[i].style.display = "block";
+        }
+
+        e.target.classList.add("active");
+    });
 }
